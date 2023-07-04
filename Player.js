4c34d7aeddcd112ec366c7796237ee7d018f9ae1
Matthew Giles <matty92g@hotmail.co.uk>
2023-07-04 21:58:26 +0100

    Added Movement to Player

--- Player.js
+++ Player.js
@@ -1,29 +1,62 @@
+import { gameVar } from './gameVar.js';
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 ctx.imageSmoothingEnabled = false;
+
+canvas.width = gameVar.canvasSize.width;
+canvas.height = gameVar.canvasSize.height;
 
 export class Player {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.rotation = 0;
   }
   draw() {
+    ctx.save();
+
+    ctx.translate(this.position.x, this.position.y);
+    ctx.rotate(this.rotation);
+    ctx.translate(-this.position.x, -this.position.y);
+
     ctx.beginPath();
     ctx.moveTo(this.position.x + 30, this.position.y);
     ctx.lineTo(this.position.x - 10, this.position.y - 10);
     ctx.lineTo(this.position.x - 10, this.position.y + 10);
-    ctx.closePath();
-
     ctx.fillStyle = 'white';
     ctx.fill();
+    ctx.closePath();
 
     ctx.beginPath();
     ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
     ctx.fillStyle = 'blue';
     ctx.fill();
     ctx.closePath();
+    ctx.restore();
   }
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+  getVertices() {
+    const cos = Math.cos(this.rotation);
+    const sin = Math.sin(this.rotation);
+
+    return [
+      {
+        x: this.position.x + cos * 30 - sin * 0,
+        y: this.position.y + sin * 30 + cos * 0,
+      },
+      {
+        x: this.position.x + cos * -10 - sin * 10,
+        y: this.position.y + sin * -10 + cos * 10,
+      },
+      {
+        x: this.position.x + cos * -10 - sin * -10,
+        y: this.position.y + sin * -10 + cos * -10,
+      },
+    ];
   }
 }
--- index.js
+++ index.js
@@ -16,4 +16,97 @@ const player = new Player({
   velocity: { x: 0, y: 0 },
 });
 
-player.update();
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  shift: {
+    pressed: false,
+  },
+};
+
+const SPEED = 3;
+const ROTATIONAL_SPEED = 0.05;
+const FRICTION = 0.99;
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.fillStyle = gameVar.backgroundColour;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+  if (keys.shift.pressed && keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED * 1.5;
+    player.velocity.y = Math.sin(player.rotation) * SPEED * 1.5;
+  } else if (keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED;
+    player.velocity.y = Math.sin(player.rotation) * SPEED;
+  } else if (!keys.w.pressed && keys.s.pressed) {
+    player.velocity.x *= FRICTION / 1.05;
+    player.velocity.y *= FRICTION / 1.05;
+  } else if (!keys.w.pressed) {
+    player.velocity.x *= FRICTION;
+    player.velocity.y *= FRICTION;
+  }
+
+  if (keys.d.pressed) {
+    player.rotation += ROTATIONAL_SPEED;
+  } else if (keys.a.pressed) {
+    player.rotation -= ROTATIONAL_SPEED;
+  }
+}
+
+animate();
+
+window.addEventListener('keydown', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = true;
+      break;
+    case 'KeyA':
+      keys.a.pressed = true;
+      break;
+    case 'KeyD':
+      keys.d.pressed = true;
+      break;
+    case 'KeyS':
+      keys.s.pressed = true;
+      break;
+    case 'ShiftLeft':
+      keys.shift.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = false;
+      break;
+    case 'KeyA':
+      keys.a.pressed = false;
+      break;
+    case 'KeyD':
+      keys.d.pressed = false;
+      break;
+    case 'KeyS':
+      keys.s.pressed = false;
+      break;
+    case 'ShiftLeft':
+      keys.shift.pressed = false;
+      break;
+  }
+});
+
+window.addEventListener('keydown', (event) => {
+  console.log(event);
+});
