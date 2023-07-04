c032bd1d0f1bbd1f3ad96eed6274c0af306ca57f
Matthew Giles <matty92g@hotmail.co.uk>
2023-07-04 19:51:04 +0100

    Player Class Created

---
+++ Player.js
@@ -0,0 +1,6 @@
+export class Player {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { gameVar } from './gameVar.js';
+import { Player } from './Player.js';
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -9,3 +10,10 @@ canvas.height = gameVar.canvasSize.height;
 
 ctx.fillStyle = gameVar.backgroundColour;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const player = new Player({
+  position: { x: canvas.width / 2, y: canvas.height / 2 },
+  velocity: { x: 0, y: 0 },
+});
+
+console.log(player);
