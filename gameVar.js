f108e4c98ca8bbe12cf01f7b043a17be1e664e72
Matthew Giles <matty92g@hotmail.co.uk>
2023-07-15 18:04:02 +0100

    Created Variable for Projectile

--- gameVar.js
+++ gameVar.js
@@ -1,4 +1,8 @@
 export const gameVar = {
   backgroundColour: 'black',
   canvasSize: { width: 500, height: 500 },
+  player: {
+    movement: { acceleration: 0.2, max: 3, rotate: 0.05, friction: 0.99 },
+    projectile: { speed: 3, rate: 1, max: 5 },
+  },
 };
