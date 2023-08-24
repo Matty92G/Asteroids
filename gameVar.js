343f5346b4298865cb73de4d148a7e6b5b784248
Matthew Giles <matty92g@hotmail.co.uk>
2023-08-24 14:56:52 +0100

    Added Variable for Rate of Asteroids

--- gameVar.js
+++ gameVar.js
@@ -5,4 +5,7 @@ export const gameVar = {
     movement: { acceleration: 0.2, max: 3, rotate: 0.05, friction: 0.99 },
     projectile: { speed: 3, rate: 1, max: 5 },
   },
+  asteroid: {
+    gameSpeed: 3000,
+  },
 };
