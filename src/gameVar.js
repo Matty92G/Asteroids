283940c0b97946b3d558a63faa16ad5f1e20fac8
Matthew Giles <matty92g@hotmail.co.uk>
2023-10-02 21:05:06 +0100

    Canvas Changed to 700

--- src/gameVar.js
+++ src/gameVar.js
@@ -1,6 +1,6 @@
 export const gameVar = {
   backgroundColour: 'black',
-  canvasSize: { width: 500, height: 500 },
+  canvasSize: { width: 700, height: 700 },
   player: {
     movement: { acceleration: 0.2, max: 2, rotate: 0.05, friction: 0.99 },
     projectile: { speed: 3, rate: 1, max: 5 },
